beee9806f5c09522de07f4b4325c532ff3cb2636
HungryDonguri <hungrydonguri@gmail.com>
2025-04-27 10:34:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.querySelector('h1').innerText = 'Redirecting...';
 const userLang = navigator.language || navigator.userLanguage;
 if (userLang.startsWith('ja')) {
     document.querySelector('p').innerHTML = 'If you cannot redirect, click <a>here</a>.';
-    document.querySelector('a').href = '/ja';
-    window.location.href = '/ja';
+    document.querySelector('a').href = 'ja/';
+    window.location.href = 'ja';
 } else {
     document.querySelector('p').innerHTML = 'If you cannot redirect, click <a>here</a>.';
-    document.querySelector('a').href = 'ja/';
+    document.querySelector('a').href = 'en/';
     window.location.href = 'en/';
 }
